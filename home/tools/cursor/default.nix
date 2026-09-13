8ce504dadbdc8cafb3dc77a13c8f64d0ad642d89
Crocus <crocus_nb@proton.com>
2026-09-13 16:10:05 +0900

    cursor: del unnecessary settings

--- home/tools/cursor/default.nix
+++ home/tools/cursor/default.nix
@@ -2,7 +2,6 @@
 {
   home.pointerCursor =
     let
-      size = 64;
       nordzy = pkgs.fetchzip {
         url = "https://github.com/guillaumeboehm/Nordzy-cursors/releases/download/v2.4.0/Nordzy-cursors.tar.gz";
         hash = "sha256-CM1trBf0TjmpyAtUwrhzxeBv5ZWRQ9bEiLlQsfOT3vE=";
@@ -12,14 +11,9 @@
       gtk.enable = true;
       x11.enable = true;
       name = "Nordzy-cursors";
-      inherit size;
       package = pkgs.runCommand "nordzy-cursor" { } ''
         mkdir -p $out/share/icons
         ln -s ${nordzy} $out/share/icons/Nordzy-cursors
       '';
     };
-
-  home.sessionVariables = {
-    XCURSOR_SIZE = "48";
-  };
 }
